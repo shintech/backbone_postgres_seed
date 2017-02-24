cc5303c197a342c022769440f8641f4c428920a3
Michael Prather <mprather1@gmail.com>
2017-02-24 15:06:33 +0000

    added test to install.sh and updated node-sass; did not fix node-sass error

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-if [ -d "node_modules" ]; then # check if node modules or build exists and remove
+if [ -d "node_modules" ] || [ -d "build" ]; then # check if node modules or build exists and remove
   echo "node_modules exists, removing..."
   rm -rv node_modules build --force
 fi
 
-mkdir build
-mkdir build/static
-cp -rv resources build
-npm cache clear
+mkdir build && \
+mkdir build/static && \
+cp -rv resources build && \
+npm cache clear && \
 npm install
-npm rebuild node-sass #error with node-sass / workaround
-bower install
-webpack
-npm run babel:build
+
+echo "workaround for node-sass error" && \
+npm rebuild node-sass && \
+
+bower install && \
+webpack && \
+npm run babel:build && \
+npm test
