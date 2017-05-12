d1276a0e3f77b42037edc3d5bbd3a2812d6f6350
Michael Prather <mprather1@gmail.com>
2017-05-12 16:32:57 +0000

    moment.js

--- app/views/ModelView.js
+++ app/views/ModelView.js
@@ -1,7 +1,18 @@
+import moment from 'moment'
+
 const ModelView = Backbone.Marionette.View.extend({
   tagName: 'tr',
   className: 'table-row',
-  template: require('../templates/model-view-template.html')
+  template: require('../templates/model-view-template.html'),
+  serializeData: function () {
+    const created_at = moment(this.model.get('created_at'))
+    
+    return {
+      'name': this.model.get('name'),
+      'attribute': this.model.get('attribute'),
+      'created_at': created_at.format('M-DD-YYYY h:mm:ss A')
+    }
+  }
 })
 
 export default ModelView
