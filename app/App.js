1bb14ffd4b0976a0203bcbc3a9b609e754e7d0a2
Michael Prather <mprather1@gmail.com>
2017-05-14 19:13:54 +0000

    use root_view package

--- app/App.js
+++ app/App.js
@@ -1,12 +1,12 @@
 import Backbone from 'backbone'
 import Marionette from 'marionette'
-import RootView from './views/RootView'
+import RootView from 'root_view'
 import Router from './router'
 
 const App = Marionette.Application.extend({
   region: 'body',
   onStart: function () {
-    this.view = new RootView()
+    this.view = new RootView({ title: 'Default' })
     this.showView(this.view)
     this.Router = new Router({ app: this })
     Backbone.history.start()
