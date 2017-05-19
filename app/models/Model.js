b8cc9ea61ede84eb81808ae4a4ef77dc7635b815
Michael Prather <mprather1@gmail.com>
2017-05-19 23:00:45 +0000

    parse model

--- app/models/Model.js
+++ app/models/Model.js
@@ -1,4 +1,5 @@
 import validation from 'backbone-validation' // eslint-disable-line
+import moment from 'moment'
 
 const Model = Backbone.Model.extend({
   urlRoot: 'http://shintech.ninja:8000/api/models',
@@ -16,6 +17,19 @@ const Model = Backbone.Model.extend({
         msg: 'Attribute must be a number...'
       }
     ]
+  },
+
+  parse: function (response) {
+    const createdAt = moment(response.created_at)
+
+    const object = {
+      id: response.id,
+      name: response.name,
+      attribute: response.attribute,
+      created_at: createdAt.format('YYYY-MM-DD hh:mm:ss A')
+    }
+
+    return object
   }
 })
 
--- app/views/LoginView.js
+++ app/views/LoginView.js
@@ -1,7 +1,7 @@
-var LoginView = Backbone.Marionette.View.extend({
+const LoginView = Backbone.Marionette.View.extend({
   tagName: 'div',
   className: 'panel panel-primary',
   template: require('../templates/login-view-template.html')
 })
 
-module.exports = LoginView
+export default LoginView
