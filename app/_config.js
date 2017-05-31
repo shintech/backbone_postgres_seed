e6008bfcfd334732124723ddcc5a80738bc847a3
Michael Prather <mprather1@gmail.com>
2017-05-31 23:06:51 +0000

    change development port

--- app/_config.js
+++ app/_config.js
@@ -1,7 +1,7 @@
 const config = {}
 
 config.development = {
-  url: 'https://' + (window.location.hostname) + '/'
+  url: 'https://' + (window.location.hostname) + ':8443/'
 }
 
 config.production = {
